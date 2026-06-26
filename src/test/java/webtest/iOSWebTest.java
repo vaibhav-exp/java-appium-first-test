ee274a3ab428d9ab0286c380d4a54602d47acec3
Vaibhav Sharma <58463601+vaibhav-exp@users.noreply.github.com>
2026-06-26 12:00:03 +0530

    CloudURL from Env

--- src/test/java/webtest/iOSWebTest.java
+++ src/test/java/webtest/iOSWebTest.java
@@ -24,7 +24,8 @@ public class iOSWebTest {
     IOSDriver driver = null;
     DesiredCapabilities dc = new DesiredCapabilities();
     String testName = "Testing Website on iOS Safari with Java";
-    String accessKey = System.getenv("SEETEST_IO_ACCESS_KEY");
+    String accessKey = System.getenv("ACCESS_KEY");
+    String cloudURL = System.getenv("CLOUD_URL");
 
     @Before
     public void setUp() throws MalformedURLException {
@@ -33,7 +34,7 @@ public class iOSWebTest {
         dc.setCapability("deviceQuery", "@os='ios' and @category='PHONE'");
         dc.setBrowserName(MobileBrowserType.SAFARI);
         dc.setCapability("autoDismissAlerts", true);
-        driver = new IOSDriver(new URL("https://uscloud.experitest.com/wd/hub"),dc);
+        driver = new IOSDriver(new URL(cloudURL + "/wd/hub"),dc);
     }
 
     @Test
