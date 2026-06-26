2e9d2b1fc61875a18ae2fe8f0f39cf48a6e77383
Vaibhav Sharma <58463601+vaibhav-exp@users.noreply.github.com>
2026-06-26 14:06:17 +0530

    Added missing dependency

--- src/test/java/webtest/AndroidWebTest.java
+++ src/test/java/webtest/AndroidWebTest.java
@@ -16,6 +16,7 @@ import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.TimeUnit;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
