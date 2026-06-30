35c5ec14fb887c19ae146a3a56f65a686a59b60a
Vaibhav Sharma <vaibhaviitk1@gmail.com>
2026-06-30 07:01:41 +0530

    Added new test

---
+++ src/test/java/apptest/TestHiddenSteps.java
@@ -0,0 +1,68 @@
+package apptest;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.ScreenOrientation;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import com.experitest.appium.SeeTestClient;
+
+public class TestHiddenSteps {
+
+    AndroidDriver driver = null;
+    DesiredCapabilities dc = new DesiredCapabilities();
+    SeeTestClient seetest;
+    String testName = "Testing Android App with Java - Hidden Steps";
+    String accessKey = System.getenv("ACCESS_KEY");
+    String cloudURL = System.getenv("CLOUD_URL");
+
+    @Before
+    public void setUp() throws IOException {
+        dc.setCapability("testName", testName);
+        dc.setCapability("accessKey", accessKey);
+        dc.setCapability("deviceQuery", "@os='android' and @category='PHONE'");
+        dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.ExperiBank/.LoginActivity");
+        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.ExperiBank");
+        dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".LoginActivity");
+        driver = new AndroidDriver(new URL( cloudURL + "/wd/hub"), dc);
+        seetest = new SeeTestClient(driver);
+    }
+
+    @Test
+    public void testYourAndroidApp() {
+        driver.rotate(ScreenOrientation.PORTRAIT);
+        driver.findElement(By.xpath("//*[@id='usernameTextField']")).sendKeys("company");
+        driver.hideKeyboard();
+        driver.findElement(By.xpath("//*[@id='passwordTextField']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@id='loginButton']")).click();
+        driver.findElement(By.xpath("//*[@id='makePaymentButton']")).click();
+        seetest.setShowReport(false);
+        driver.findElement(By.xpath("//*[@id='phoneTextField']")).sendKeys("0541234567");
+        driver.findElement(By.xpath("//*[@id='nameTextField']")).sendKeys("Jon Snow");
+        seetest.setShowReport(true);
+        driver.findElement(By.xpath("//*[@id='amountTextField']")).sendKeys("50");
+        driver.findElement(By.xpath("//*[@id='countryButton']")).click();
+        driver.findElement(By.xpath("//*[@text='Switzerland']")).click();
+        driver.findElement(By.xpath("//*[@id='sendPaymentButton']")).click();
+        driver.findElement(By.xpath("//*[@text='Yes']")).click();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null)
+        {
+            driver.quit();
+            System.out.println("Report URL : " + driver.getCapabilities().getCapability("digitalai:reportUrl"));
+
+        }
+    }
+
+}
